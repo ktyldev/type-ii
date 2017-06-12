c6602bc14deaf0283ad87df73390a22f729a186a
Cat Flynn <mewtwo97@hotmail.com>
2017-06-12 22:16:14 +0100

    Smooth camera movement

--- Assets/Areas/Camera/CameraController.cs
+++ Assets/Areas/Camera/CameraController.cs
@@ -22,31 +22,7 @@ public class CameraController : MonoBehaviour {
     private float _zoom;
 
     private Transform _target;
-
-    private float GetVerticalAxis() {
-        if (enablePanning) {
-            if (Input.mousePosition.y > Screen.height - panMargin) {
-                return 1;
-            } else if (Input.mousePosition.y < panMargin) {
-                return -1;
-            }
-        }
-
-        return Input.GetAxis("Vertical");
-    }
-
-    private float GetHorizontalAxis() {
-        if (enablePanning) {
-            if (Input.mousePosition.x > Screen.width - panMargin) {
-                return 1;
-            } else if (Input.mousePosition.x < panMargin) {
-                return -1;
-            }
-        }
-
-        return Input.GetAxis("Horizontal");
-    }
-
+    
     private void Start() {
         _distance = initialDistance;
         _zoom = _distance;
@@ -70,10 +46,6 @@ public class CameraController : MonoBehaviour {
             Move(Vector3.right, h);
         }
     }
-    
-    private void Move(Vector3 direction, float axis) {
-        focus.transform.Translate(direction * axis * Mathf.Sqrt(_distance) * movementSensitivity * Time.deltaTime);
-    }
 
     private void LateUpdate() {
         if (focus == null)
@@ -84,16 +56,41 @@ public class CameraController : MonoBehaviour {
         var dir = new Vector3(0, 0, -_distance);
         var rotation = Quaternion.Euler(angle, 0, 0);
 
-        transform.position = focus.position + rotation * dir;
-        transform.LookAt(focus.position);
+        var currentFocus = _target ?? focus;
+        transform.position = currentFocus.position + rotation * dir;
+        transform.LookAt(currentFocus.position);
     }
 
-    public float GetCurrentDistanceFromTarget() {
-        return _distance;
+    private void Move(Vector3 direction, float axis) {
+        focus.transform.Translate(direction * axis * Mathf.Sqrt(_distance) * movementSensitivity * Time.deltaTime);
     }
 
     public void Track(Transform target, float distance) {
         _target = target;
         _zoom = distance;
+    }
+
+    private float GetVerticalAxis() {
+        if (enablePanning) {
+            if (Input.mousePosition.y > Screen.height - panMargin) {
+                return 1;
+            } else if (Input.mousePosition.y < panMargin) {
+                return -1;
+            }
+        }
+
+        return Input.GetAxis("Vertical");
+    }
+
+    private float GetHorizontalAxis() {
+        if (enablePanning) {
+            if (Input.mousePosition.x > Screen.width - panMargin) {
+                return 1;
+            } else if (Input.mousePosition.x < panMargin) {
+                return -1;
+            }
+        }
+
+        return Input.GetAxis("Horizontal");
     }
 }
