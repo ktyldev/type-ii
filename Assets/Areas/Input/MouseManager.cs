3cfd435dd6565acaa7d3489c1c833cb632b8ce15
Cat Flynn <mewtwo97@hotmail.com>
2017-06-14 00:27:56 +0100

    Add basic mouse selection

---
+++ Assets/Areas/Input/MouseManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+    public GameObject selectedObject;
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                var hitObject = hit.transform.gameObject;
+                var root = hit.transform.root.gameObject;
+
+                while (hitObject.GetComponent<KeplerTreeNode>() == null) {
+                    if (hitObject == root)
+                        return;
+
+                    hitObject = hitObject.transform.parent.gameObject;
+                }
+
+                SelectObject(hitObject);
+            } else {
+                ClearSelection();
+            }
+        }
+    }
+
+    private void SelectObject(GameObject go) {
+        selectedObject = go;
+    }
+
+    private void ClearSelection() {
+        selectedObject = null;
+    }
+}
