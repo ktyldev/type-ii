220ad630bbe28ceae1f63bcc2032df2fbcb9a29d
Cat Flynn <mewtwo97@hotmail.com>
2017-11-27 22:45:07 +0000

    Make selection window draggable

---
+++ Assets/Areas/UI/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public RectTransform panelRoot;
+
+    private Vector3 _dragOffset;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _dragOffset = Input.mousePosition - panelRoot.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        panelRoot.position = Input.mousePosition - _dragOffset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _dragOffset = Vector3.zero;
+    }
+}
